e0138fe917949a00a860a663f4129d47dd3c4388
Anthony Fodor <anthony.fodor@gmail.com>
2016-06-15 19:40:49 -0400

    stub to format multiple databases....

---
+++ src/homologySearch/blast/FormatMultipleBlastDatabases.java
@@ -0,0 +1,16 @@
+package homologySearch.blast;
+
+public class FormatMultipleBlastDatabases
+{
+	/*
+	 * Takes in FASTA_DIR_TO_FORMAT which should only contain fasta files
+	 * (sub-directories are allowed but will be ignored)
+	 * 
+	 * Writes scripts to SCRIPTS_DIR_FOR_BLAST_FORMAT
+	 */
+
+	public static void main(String[] args) throws Exception
+	{
+		
+	}
+}
