90f86c6461ff285c170f946008d2b200671b850d
Anthony Fodor <anthony.fodor@gmail.com>
2016-06-25 21:50:00 -0400

    implementaion of fisher test...

--- src/utils/FisherTest.java
+++ src/utils/FisherTest.java
@@ -2,9 +2,25 @@ package utils;
 
 public class FisherTest
 {
-	public static double choose(int n, int k) throws Exception
+	
+	public static double choose(int n, int k) 
 	{
 		return Math.exp(Functions.lnfgamma(n + 1) - Functions.lnfgamma(k+1) - Functions.lnfgamma(n-k+1));
+	}
+	
+	public static double getFisherP( int allN, int allK, int n, int k )
+	{
+		return choose(allK,k) * choose(allN - allK, n - k) / choose(allN,n);
+	}
+	
+	public static double getFisherPSum(int allN, int allK, int n, int k  )
+	{
+		double sum =0;
+		
+		for( int x = k; x <=n ; x++)
+			sum += getFisherP( allN, allK, n, x);
+		
+		return sum;
 	}
 	
 	public static void main(String[] args) throws Exception
@@ -13,6 +29,10 @@ public class FisherTest
 		
 		System.out.println(choose(300,12));
 		
+		System.out.println(choose(600,234));
 		
+		System.out.println( getFisherP(60, 20, 7, 3) );
+		
+		System.out.println( getFisherPSum(34, 16, 15, 13));
 	}
 }
