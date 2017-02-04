c5c57f229a43a9436a5a908bff89430d06313e1f
mikesioda <mikesioda@gmail.com>
2017-02-04 12:15:51 -0500

    Version 1.0 - First Deployment

--- cluster_build.sh
+++ cluster_build.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-# remove old build & then build/test
-rm -rf ./BioLockJ
-git clone https://github.com/mikesioda/BioLockJ.git
-sleep 4s
-cd ./BioLockJ
+
+# build/test
+
+# git clone https://github.com/mikesioda/BioLockJ.git
+# sleep 4s
+#cd ./BioLockJ
 ant
-java -cp ./lib/*:./bin bioLockJ.BioLockJ ./resources/allMiniKraken/krakenAdenonas2015
+
+java -cp ./lib/*:./bin bioLockJ.BioLockJ ./resources/obesityFeb2017.properties
+#java -cp ./lib/*:./bin bioLockJ.BioLockJ ./resources/allMiniKraken/krakenAdenonas2015
 #java -cp ./lib/*:./bin bioLockJ.BioLockJ ./resources/findResistance/FindCRE_Resistance.BioLockJProperties
